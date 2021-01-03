709b2f315f90058ca7caea3f702244d4ad57c5aa
YTPGozde <76629684+YTPGozde@users.noreply.github.com>
2021-01-03 15:31:59 +0300

    PY&JOB
    Commentlerin olduğu örnek python dosyası ve kullanıldığında oluşan job dosyası bulunmaktadır.

---
+++ Ornek_py&job/Ornek.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  3 15:24:12 2021
+
+@author: Gözde
+"""
+#NuNuTRUBA_KUYRUK=short:
+#NuNuTRUBA_CPUSAYISI=4:
+#NuNuTRUBA_GPUSAYISI=1:
+#NuNuTRUBA_PIP=numpy,sys:
+#NuNuTRUBA_PARAMETRELER=1,2,3:
+#NuNuTRUBA_GIDENDOSYALAR=dosya1,dosya2:
+#NuNuTRUBA_DONENDOSYALAR=dosya,dosya:
